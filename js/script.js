f7ee8352c457490ca2b27e1cc4731dde8582191d
Paulina Kolonczyk <paulinakolonczyk@interia.pl>
2021-02-07 13:56:03 +0100

    Add game result

--- js/script.js
+++ js/script.js
@@ -33,3 +33,34 @@ else if(playerInput == '3'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+}
+else if( computerMove == 'kamień' && playerMove == 'nożyce'){
+  printMessage('Ja wygrywam!');
+}
+else if( computerMove == 'kamień' && playerMove == 'kamień'){
+  printMessage('Remis!');
+}
+else if(computerMove == 'papier' && playerMove == 'papier'){
+  printMessage('Remis!');
+}
+else if(computerMove == 'papier' && playerMove == 'kamień'){
+  printMessage('Ja wygrywam!');
+}
+else if(computerMove == 'papier' && playerMove == 'nożyce'){
+  printMessage('Ty wygrywasz!');
+}
+else if(computerMove == 'nożyce' && playerMove == 'papier'){
+  printMessage('Ja wygrywam!');
+}
+else if(computerMove == 'nożyce' && playerMove == 'nożyce'){
+  printMessage('Remis!');
+}
+else if(computerMove == 'nożyce' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!');
+}
+else{
+  printMessage('błędny ruch!');
+}
